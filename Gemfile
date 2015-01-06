8b5964636808f328ac3a22e037fd21696d9de56a
tim geaney <timgeaney24@gmail.com>
2015-01-06 17:07:35 +0000

    setup controlers, models and routes

--- Gemfile
+++ Gemfile
@@ -32,6 +32,16 @@ group :doc do
   gem 'sdoc', require: false
 end
 
+gem 'bootstrap-sass', '~> 3.2.0'
+
+gem 'autoprefixer-rails'
+
+gem 'bcrypt-ruby', :require => "bcrypt"
+
+gem 'mail_form'
+
+gem 'simple_form'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,121 @@
-RailsBlog::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+Rails.application.routes.draw do
+  get 'contact' => 'contacts#new'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  get 'contacts/create'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get 'about' => 'about#index'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  namespace :admin do
+  get 'sessions/new'
+  end
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  namespace :admin do
+  get 'sessions/create'
+  end
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  namespace :admin do
+  get 'users/new'
+  end
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  namespace :admin do
+  get 'users/create'
+  end
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+  namespace :admin do
+  get 'users/edit'
+  end
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+  namespace :admin do
+  get 'users/update'
+  end
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  namespace :admin do
+  get 'users/destroy'
+  end
+
+  namespace :admin do
+  get 'users/index'
+  end
+
+  namespace :admin do
+  get 'users/show'
+  end
+
+  namespace :admin do
+  get 'comments/destroy'
+  end
+
+  get 'comments/new'
+
+  namespace :admin do
+  get 'categories/new'
+  end
+
+  namespace :admin do
+  get 'categories/create'
+  end
+
+  namespace :admin do
+  get 'categories/edit'
+  end
+
+  namespace :admin do
+  get 'categories/update'
+  end
+
+  namespace :admin do
+  get 'categories/destroy'
+  end
+
+  namespace :admin do
+  get 'categories/index'
+  end
+
+  namespace :admin do
+  get 'categories/show'
+  end
+
+  get 'categories/show'
+
+  namespace :admin do
+  get 'posts/new'
+  end
+
+  namespace :admin do
+  get 'posts/create'
+  end
+
+  namespace :admin do
+  get 'posts/edit'
+  end
+
+  namespace :admin do
+  get 'posts/update'
+  end
+
+  namespace :admin do
+  get 'posts/destroy'
+  end
+
+  namespace :admin do
+  get 'posts/index'
+  end
+
+  namespace :admin do
+  get 'posts/show'
+  end
+
+  get 'posts/index'
+
+  get 'posts/show'
+
+  resources :posts, :categories, :comments
+  resources "contacts", only: [:new, :create]
+
+  namespace :admin do
+    resources :posts, :categories, :comments, :users
+  end
+
+  root 'posts#index'
+
 end
