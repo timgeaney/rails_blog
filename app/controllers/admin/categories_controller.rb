8ab4f202ace101c20c97a169be900ee094cb6e1e
tim geaney <timgeaney24@gmail.com>
2015-01-07 11:10:27 +0000

    admin crud

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,23 +1,50 @@
 class Admin::CategoriesController < Admin::ApplicationController
   def new
+    @page_title = "Add Category"
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = 'Category Created'
+      redirect_to admin_categories_path
+    else
+      render "new"
+    end
   end
 
   def edit
+    @category = Category.find(params[:id])
   end
 
   def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = 'category Updated'
+      redirect_to admin_categories_path
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    flash[:notice] = 'Category Removed'
+    redirect_to admin_categories_path
   end
 
   def index
     @categories = Category.all
+
   end
 
   def show
+  end
+
+  private 
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
